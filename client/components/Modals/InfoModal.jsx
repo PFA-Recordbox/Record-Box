ac35d9238af68cf5da2ccb6599715a7d45f65fbc
Justin Poirier <poirierj94@gmail.com>
2023-05-15 11:53:29 -0500

    starting info modal

---
+++ client/components/Modals/InfoModal.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function InfoModal({ showInfoModal, setShowInfoModal, }) {
+  if (!showInfoModal) {
+    return (null);
+  }
+
+  
+
+}
+
+export default InfoModal
