27ea45b69f4b01eaef2d3552a30df2faa3c3baff
sergeylvq <sergeylvq@gamil.com>
2023-05-15 15:53:48 -0400

    updated controllers - not final

--- server/model/dataModel.js
+++ server/model/dataModel.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const dataSchema = new Schema({
-    website: { type: String, required: true},
-    userId: { type: String, required: true},
-    password: { type: String, required: true},
-  });
-  
-  module.exports = mongoose.model('Data', dataSchema);
+  website: { type: String, required: true },
+  userId: { type: String, required: true }, // username change.
+  password: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Data", dataSchema);
